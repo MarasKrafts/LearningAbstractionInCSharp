af9dc4ce4990e6436e626d166d5e50a946bd1905
MarasKrafts <goldenprenzy@gmail.com>
2024-03-01 16:16:44 +0100

    Add project files.

---
+++ LearningAbstractionInCSharp/Pay.cs
@@ -0,0 +1,38 @@
+﻿namespace LearningAbstractionInCSharp
+{
+    public abstract class Pay
+    {
+        protected int _basicPay = 50000;
+        protected int _houseRent = 89000;
+        protected int _payRate = 5;
+        protected int _netPay;
+
+        public abstract int GradeOne { get; }
+        public abstract int GradeTwo { get; }
+    }
+
+    public class PayBill : Pay
+    {
+        public override int GradeOne
+        {
+            get
+            {
+                return _netPay = _basicPay + _houseRent;
+
+            }
+        }
+        public override int GradeTwo
+        {
+            get
+            {
+                return _netPay = _basicPay + _basicPay/_payRate + _houseRent * _payRate;
+            }
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("The output Mara was able to pay for GradeOne is: {0}", GradeOne);
+            Console.WriteLine("The output Mara was able to pay for GradeTwo is: {0}", GradeTwo);
+        }
+    }
+}
